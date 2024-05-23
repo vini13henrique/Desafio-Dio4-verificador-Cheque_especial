d2e10b75918cb2d9cc9701c033329f7bafffb5e2
vinicius henrique <viniciuslimalopes13@gmail.com>
2024-05-23 18:54:05 -0300

    Projeto Concluído

---
+++ VerificadorChequeEspecial/src/ChequeEspecial/VerificadorChequeEspecial.java
@@ -0,0 +1,33 @@
+package ChequeEspecial;
+
+import java.util.Scanner;
+
+public class VerificadorChequeEspecial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double saldo = scanner.nextDouble();
+        double saque = scanner.nextDouble();
+
+        // Define um limite para o cheque especial
+        double limiteChequeEspecial = 500;
+
+        // Verifica se o saque ultrapassa o saldo disponível
+        if (saque < saldo) {
+             System.out.println("Transacao realizada com sucesso.");
+             
+        } else if(saque > saldo+limiteChequeEspecial) { // TODO: Verificar se o saque ultrapassa o limite do cheque especial
+        
+             System.out.println("Transacao nao realizada. Limite do cheque especial excedido.");
+            // Em caso positivo, imprimir "Transacao realizada com sucesso utilizando o cheque especial."
+            
+        }else{
+          System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+          // Caso contrário, imprimir "Transacao nao realizada. Limite do cheque especial excedido."
+        }
+
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
+    }
+}
